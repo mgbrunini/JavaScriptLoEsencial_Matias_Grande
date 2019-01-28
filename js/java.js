34b522fc9632d9ae1ba21d55c882323ca560bdf4
Matias <mgbrunini@gmail.com>
2019-01-28 12:35:24 -0300

    Se crea el archivo JS y se hace el JSON

---
+++ js/java.js
@@ -0,0 +1,12 @@
+<!-- Creacion del JSON-->
+var jsonText = '{"estudiantes" : [' +
+'{"codigo" : "001"     ,"nombre" : "Julia"     ,"nota" : 9},' +
+'{"codigo" : "002"     ,"nombre" : "Matias"     ,"nota" : 10},' +
+'{"codigo" : "003"     ,"nombre" : "Isabella"     ,"nota" : 7},' +
+'{"codigo" : "004"     ,"nombre" : "Liz"     ,"nota" : 9},' +
+'{"codigo" : "005"     ,"nombre" : "Claudio"     ,"nota" : 8},' +
+'{"codigo" : "006"     ,"nombre" : "Cecilia"     ,"nota" : 3},' +
+'{"codigo" : "007"     ,"nombre" : "Cesar"     ,"nota" : 5},' +
+'{"codigo" : "008"     ,"nombre" : "Analia"     ,"nota" : 8},' +
+'{"codigo" : "009"     ,"nombre" : "Leonel"     ,"nota" : 4},' +
+'{"codigo" : "010"     ,"nombre" : "Dante"     ,"nota" : 10}' + ']}';
